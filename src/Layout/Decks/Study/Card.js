a24dc64ed6b587c5ef05815c537aee2eeaf87488
Dexter Berend <dexterjberend@gmail.com>
2021-09-28 14:29:18 -0500

    Study with only 1 card

--- src/Layout/Decks/Study/Card.js
+++ src/Layout/Decks/Study/Card.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Card() {
+function Card({ card, currentIndex, setCurrentIndex, total }) {
+    const [showFront, setShowFront] = useState(true);
+    
+    // todo: show Next button when the backside is shown
+    const flip = () => {
+        setShowFront(!showFront);
+    }
+
+    const visibleText = showFront ? card.front : card.back;
 
     return (
         <div className="row">
             <div className="card col m-3 pl-0 pr-0">
                 <div className="card-body">
-                    <h3>Card 1 of 3</h3>
-                    <p>Card Text</p>
-                    <button type="button" class="btn btn-secondary">Flip</button>
+                    <h3>Card {currentIndex + 1} of {total}</h3>
+                    <p>{visibleText}</p>
+                    <button type="button" class="btn btn-secondary" onClick={flip}>Flip</button>
                 </div>
             </div>
         </div>
--- src/Layout/Decks/Study/Study.js
+++ src/Layout/Decks/Study/Study.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { readDeck } from "../../../utils/api";
+import { listCards, readDeck } from "../../../utils/api";
 import BreadcrumbBar from "../../BreadcrumbBar";
 import Card from "./Card";
 
 function Study() {
+    const [cards, setCards] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
     const [deck, setDeck] = useState({name: "", id: 0});
     const { deckId } = useParams();
     const PAGE_NAME = "Study";
 
     useEffect(() => {
+        async function loadCards() {
+            const cardsFromAPI = await listCards(deckId);
+            setCards(cardsFromAPI);
+        }
         async function loadDeck() {
             const deckFromAPI = await readDeck(deckId);
             setDeck(deckFromAPI);
         }
+        loadCards();
         loadDeck();
     }, [deckId])
 
@@ -23,13 +30,28 @@ function Study() {
         {text: deck.name, url: `/decks/${deck.id}`},
     ]
 
-    return (
-        <div>
-            <BreadcrumbBar links={navLinks} currentPage={PAGE_NAME} />
-            <h2>{PAGE_NAME}: {deck.name}</h2>
-            <Card />
-        </div>
-    )
+    if (cards.length > 0) {
+        return (
+            <div>
+                <BreadcrumbBar links={navLinks} currentPage={PAGE_NAME} />
+                <h2>{PAGE_NAME}: {deck.name}</h2>
+                <Card 
+                    card={cards[currentIndex]} 
+                    currentIndex={currentIndex} 
+                    setCurrentIndex={setCurrentIndex} 
+                    total={cards.length} 
+                />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <BreadcrumbBar links={navLinks} currentPage={PAGE_NAME} />
+                <h2>{PAGE_NAME}: {deck.name}</h2>
+            </div>
+        );
+    }
+    
 }
 
 export default Study;
